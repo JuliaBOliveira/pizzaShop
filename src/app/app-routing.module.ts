f83a3819c8219e9b4ff23a46a8bf3e4f28d765b7
Julia Oliveira <juliabomfimdeoliveira@hotmail.com>
2023-04-24 16:55:11 -0300

    ínitial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./app.module').then((m) => m.AppModule),
+    loadChildren: () =>
+      import('./pages/menu/menu.module').then((m) => m.MenuPageModule),
   },
 ];
 
---
+++ src/app/pages/menu/menu.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { isPlatform } from '@ionic/angular';
+import categoryData from '../../../assets/company/categories.json';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+  menuItems = [
+    {
+      title: 'Home',
+      icon: 'home',
+      path: '',
+    },
+    {
+      title: 'Products',
+      icon: 'list',
+      path: '/products',
+      children: categoryData,
+    },
+    {
+      title: 'About',
+      icon: 'information',
+      path: '/about',
+    },
+  ];
+
+  title = 'Home';
+
+  constructor() {}
+
+  ngOnInit() {
+    const headerHeight = isPlatform('ios') ? 44 : 56;
+    document.documentElement.style.setProperty(
+      '--header-height',
+      `${headerHeight}px`
+    );
+  }
+
+  setTitle(title: string) {
+    this.title = title;
+  }
+}
